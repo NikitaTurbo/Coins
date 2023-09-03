7c0d270af193506e92c7018bf559f78987af5707
Nikita Turbo <117271258+NikitaTurbo@users.noreply.github.com>
2023-09-03 08:28:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const animateValue = (id, start, end, duration) => {
-    if (start === end) return;
-    var range = end - start;
-    var current = start;
-    var increment = end > start? 1 : -1;
-    var stepTime = Math.abs(Math.floor(duration / range));
-    var obj = document.getElementById(id);
-    var timer = setInterval(function() {
-        current += increment;
-        obj.innerHTML = current;
-        if (current == end) {
-            clearInterval(timer);
-        }
-    }, stepTime);
-}
-animateValue("number", 1, 1160, 1000);
+
